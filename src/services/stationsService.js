39ceff93f1d0bdb7acef904e580f1f0af9b7440d
César Álvarez <cesaralv97@gmail.com>
2025-03-29 17:32:23 +0100

    feat(stations): create stations service with tests

---
+++ src/services/stationsService.js
@@ -0,0 +1,8 @@
+import { api } from './api';
+
+export const stationsService = {
+  async getStations(query = '') {
+    const endpoint = `/stations${query ? `?search=${query}` : ''}`;
+    return api.get(endpoint);
+  }
+};
