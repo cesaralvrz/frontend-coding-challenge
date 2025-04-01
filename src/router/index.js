153717a7dadfb74db4e7e0d43233fe2e7f3b3d7b
César Álvarez <cesaralv97@gmail.com>
2025-04-01 11:31:50 +0200

    ci(pages): add GitHub Pages deployment workflow and configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import CalendarView from '~/views/CalendarView.vue'
 import BookingDetailView from '~/views/BookingDetailView.vue'
 
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/frontend-coding-challenge/'),
   routes
 })
 
